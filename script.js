5d37bdb54afa30a72db42abbf587204e0ec410dc
Sarmad Siddique <sarmadsiddique123@gmail.com>
2024-07-29 19:22:54 -0700

    Added support for multiple colors and an Eraser

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let mouseDown = false;
+let pixelColor = "green";
+let dimensions = 32;
 const sketchContainer = document.querySelector(".sketch-container");
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < dimensions; i++) {
     const rowContainer = document.createElement("div");
 
     rowContainer.classList.add("row");
@@ -9,37 +11,46 @@ for (let i = 0; i < 16; i++) {
     sketchContainer.appendChild(rowContainer);
 
 
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < dimensions; j++) {
         const pixelDiv = document.createElement("div");
         pixelDiv.classList.add("pixel");
         
         rowContainer.appendChild(pixelDiv);
 
-        pixelDiv.addEventListener("mouseenter", function (event) {
+        pixelDiv.addEventListener("mousedown", (event)=> {
+            mouseDown= true;
+
+            targetDiv = event.target;
+        
+            targetDiv.style.backgroundColor = pixelColor;
+        });
+
+        pixelDiv.addEventListener("mouseenter", (event) => {
             if (!mouseDown) {
                 return;
             }
             targetDiv = event.target;
         
-            targetDiv.style.backgroundColor = "green";
-        });
-
-        pixelDiv.addEventListener("mousedown", ()=> {
-            mouseDown= true;
-
-            targetDiv = event.target;
-        
-            targetDiv.style.backgroundColor = "green";
+            targetDiv.style.backgroundColor = pixelColor;
         });
 
         pixelDiv.addEventListener("mouseup", ()=> {
             mouseDown= false;
         });
     }
-
     
 }
 
 sketchContainer.addEventListener("mouseleave", ()=>{
     mouseDown = false;
 });
+
+const colorDiv = document.querySelectorAll(".color");
+
+colorDiv.forEach( div => {
+    div.addEventListener("click", (event) => {
+        let curTarget = event.currentTarget;
+        pixelColor = curTarget.style.backgroundColor
+    });
+});
+
